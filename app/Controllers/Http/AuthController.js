7f82ff3be2c3b232ea4894faf573249e518d9f73
Redeemer40 <victorjonah199@gmail.com>
2020-06-25 00:01:57 +0100

    add service and controller for registration

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -3,6 +3,36 @@ const { validateAll } = use("Validator");
 const users = make("App/Services/UserService");
 
 class AuthController {
+  async displayRegister({ view }) {
+    return view.render("auth.register");
+  }
+
+  async userRegister({ request, session, response }) {
+    const userData = request.only([
+      "email",
+      "password",
+      "password_confirmation",
+    ]);
+
+    const rules = {
+      email: "required|email|max:255|unique:users",
+      password: "required|min:6|max:30",
+      password_confirmation: "required_if:password|min:6|max:30|same:password",
+    };
+    const validation = await validateAll(userData, rules);
+    if (validation.fails()) {
+      session.withErrors(validation.messages()).flashExcept(["password"]);
+      return response.redirect("back");
+    }
+
+    try {
+      await users.register(userData);
+      return response.redirect("/dashboard");
+    } catch (error) {
+      session.flash({ error: "Unable to register" });
+      return response.redirect("back");
+    }
+  }
   async displayLogin({ view }) {
     return view.render("auth.login");
   }
--- app/Services/UserService.js
+++ app/Services/UserService.js
@@ -1,6 +1,17 @@
 const User = use("App/Models/User");
-
+const Hash = use("Hash");
 class UserService {
+  async register(userData) {
+    const hashedPassword = await Hash.make(userData.password);
+    const user = new User({
+      email: userData.email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    return user;
+  }
   async login(userData, auth) {
     const { email, password } = userData;
     await auth.attempt(email, password);
